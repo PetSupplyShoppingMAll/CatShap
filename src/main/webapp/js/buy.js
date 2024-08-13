f34f252a7b05ff78e9f9bbb8433dbc9f1516d670
yunju <dbswnfnal@naver.com>
2024-08-13 17:09:11 +0900

    fix : 상품 구매 화면 수정

--- src/main/webapp/js/buy.js
+++ src/main/webapp/js/buy.js
@@ -1,53 +1,40 @@
-window.onload = function(){
-
-
+$(document).ready(function () {
   // 안내 팝업창
-  $(".btn").click(function(){
+  $(".btn").click(() => {
     $("#event").show();
-  })
-  $("#event span").click(function(){
-    $("#event").hide();
-  })
-
-
-
-  // 타이틀 클릭시 위로 올라가게하기
-  const title = document.querySelectorAll('.title');
-  const content = document.querySelectorAll('.content');
-
-  title.forEach(content => {
-    content.addEventListener('click', function(){
-      const targetId = this.getAttribute('data-target');
-      const targetDiv = document.getElementById(targetId);
-      targetDiv.classList.toggle('hidden')
-    });
-  })
-
-
-
-
-
-  // 체크 박스 클릭시 모두 클릭하기
-  const allcheckbox = document.getElementById('allcheckbox');
-  const checkbox = [
-    document.getElementById('main5check1'),
-    document.getElementById('main5check2'),
-    document.getElementById('main5check3')
-  ];
-  
-  
-  allcheckbox.addEventListener('change', function(){
-    if(this.checked) {
-      checkbox.forEach(cb => cb.checked = true);
-    }else {
-      checkbox.forEach(cb => cb.checked = false);
-    }
-
   });
 
+  $("#event span").click(() => {
+    $("#event").hide();
+  });
 
+  // 주소 검색 버튼 클릭 이벤트 핸들러
+  $('#addressSearchButton').click((event) => {
+    event.preventDefault();
+    sample6_execDaumPostcode(event);
+  });
 
+  // 주소 검색 함수
+  const sample6_execDaumPostcode = (e) => {
+	e.preventDefault();
+	
+    new daum.Postcode({
+      oncomplete: (data) => {
+        $('#zipcode').val(data.zonecode);
+        $('#address1').val(data.address);
+      }
+    }).open();
+  };
 
-}
+  // 타이틀 클릭 시 위로 올라가게 하기
+  $('.title').click(function () {
+    const targetId = $(this).data('target');
+    $(`#${targetId}`).toggleClass('hidden');
+  });
 
-
+  // 체크 박스 클릭 시 모두 클릭하기
+  $('#allcheckbox').change(function () {
+    const isChecked = $(this).is(':checked');
+    $('#main5check1, #main5check2, #main5check3').prop('checked', isChecked);
+  });
+});
